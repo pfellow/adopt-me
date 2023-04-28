f331c9a9cb2c872645098f17516d3dbac928c509
pfellow <pfellow@gmail.com>
2023-04-28 23:05:35 +1200

    completed until Error Boundaries

---
+++ src/ErrorBoundary.jsx
@@ -0,0 +1,9 @@
+import { Component } from "react";
+import { Link } from "react-router-dom";
+
+class ErrorBoundary extends Component {
+  state = { hasError: false };
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+}
--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,25 +1,76 @@
 import { useState } from "react";
+import useBreedList from "./useBreedList";
+import { useQuery } from "@tanstack/react-query";
+import fetchSearch from "./fetchSearch";
+import Results from "./Results";
+
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
-  const [location, setLocation] = useState("Seattle, WA");
-  const locationChangeHandler = (event) => {
-    setLocation(event.target.value);
+  const [requestParams, setRequestParams] = useState({
+    location: "",
+    animal: "",
+    breed: "",
+  });
+  const [animal, setAnimal] = useState("");
+  const [breeds] = useBreedList(animal);
+
+  const results = useQuery(["search", requestParams], fetchSearch);
+  const pets = results?.data?.pets ?? [];
+
+  const animalChangeHandler = (event) => {
+    setAnimal(event.target.value);
   };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    const obj = {
+      animal: formData.get("animal") ?? "",
+      breed: formData.get("breed") ?? "",
+      location: formData.get("location") ?? "",
+    };
+    setRequestParams(obj);
+  };
+
   return (
     <div className="search-params">
-      <form action="">
+      <form action="" onSubmit={submitHandler}>
         <label htmlFor="location">
           Location
           <input
             id="location"
-            value={location}
+            name="location"
             placeholder="location"
             type="text"
-            onChange={locationChangeHandler}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select id="animal" value={animal} onChange={animalChangeHandler}>
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select id="breed" name="breed" disabled={breeds.length === 0}>
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
         </label>
         <button type="submit">Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
